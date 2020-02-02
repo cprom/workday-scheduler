4a4a2726f69960700f7a390eeea78e7721e2e527
cprom <chen.prom@gmail.com>
2020-02-01 19:39:19 -0800

    Build logic for text input

--- assets/app.js
+++ assets/app.js
@@ -130,3 +130,138 @@ if (fivePm == currentTime) {
 
 }
 
+//store to local storage
+
+//9AM ----------------------
+document.getElementById('button-nine-am').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-nine-am').value
+  localStorage.setItem('notes', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-nine-am').append(items)
+  console.log(textAreaNotes)
+})
+
+let items = JSON.parse(localStorage.getItem('notes'))
+if (items) {
+  document.getElementById('text-area-nine-am').innerHTML = items
+}
+
+//10AM ----------------------
+document.getElementById('button-ten-am').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-ten-am').value
+  localStorage.setItem('notesTen', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-ten-am').append(itemsTen)
+  console.log(textAreaNotes)
+})
+
+let itemsTen = JSON.parse(localStorage.getItem('notesTen'))
+if (itemsTen) {
+  document.getElementById('text-area-ten-am').innerHTML = itemsTen
+}
+
+//11AM ------------------------
+
+document.getElementById('button-eleven-am').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-eleven-am').value
+  localStorage.setItem('notesEleven', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-eleven-am').append(itemsEleven)
+  console.log(textAreaNotes)
+})
+
+let itemsEleven = JSON.parse(localStorage.getItem('notesEleven'))
+if (itemsEleven) {
+  document.getElementById('text-area-eleven-am').innerHTML = itemsEleven
+}
+
+//12PM ------------------------
+
+document.getElementById('button-twelve-pm').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-twelve-pm').value
+  localStorage.setItem('notesTwelve', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-twelve-pm').append(itemsTwelve)
+  console.log(textAreaNotes)
+})
+
+let itemsTwelve = JSON.parse(localStorage.getItem('notesTwelve'))
+if (itemsTwelve) {
+  document.getElementById('text-area-twelve-pm').innerHTML = itemsTwelve
+}
+
+//1PM ------------------------
+
+document.getElementById('button-one-pm').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-one-pm').value
+  localStorage.setItem('notesOne', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-one-pm').append(itemsOne)
+  console.log(textAreaNotes)
+})
+
+let itemsOne = JSON.parse(localStorage.getItem('notesOne'))
+if (itemsOne) {
+  document.getElementById('text-area-one-pm').innerHTML = itemsOne
+}
+
+//2PM ------------------------
+
+document.getElementById('button-two-pm').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-two-pm').value
+  localStorage.setItem('notesTwo', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-two-pm').append(itemsTwo)
+  console.log(textAreaNotes)
+})
+
+let itemsTwo = JSON.parse(localStorage.getItem('notesTwo'))
+if (itemsTwo) {
+  document.getElementById('text-area-two-pm').innerHTML = itemsTwo
+}
+
+//3PM ------------------------
+
+document.getElementById('button-three-pm').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-three-pm').value
+  localStorage.setItem('notesThree', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-three-pm').append(itemsThree)
+  console.log(textAreaNotes)
+})
+
+let itemsThree = JSON.parse(localStorage.getItem('notesThree'))
+if (itemsThree) {
+  document.getElementById('text-area-three-pm').innerHTML = itemsThree
+}
+
+//4PM ------------------------
+
+document.getElementById('button-four-pm').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-four-pm').value
+  localStorage.setItem('notesFour', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-four-pm').append(itemsFour)
+  console.log(textAreaNotes)
+})
+
+let itemsFour = JSON.parse(localStorage.getItem('notesFour'))
+if (itemsFour) {
+  document.getElementById('text-area-four-pm').innerHTML = itemsFour
+}
+
+//5PM ------------------------
+
+document.getElementById('button-five-pm').addEventListener('click', event => {
+  event.preventDefault()
+  let textAreaNotes = document.getElementById('text-area-five-pm').value
+  localStorage.setItem('notesFive', JSON.stringify(textAreaNotes))
+  document.getElementById('text-area-five-pm').append(itemsFive)
+  console.log(textAreaNotes)
+})
+
+let itemsFive = JSON.parse(localStorage.getItem('notesFive'))
+if (itemsFive) {
+  document.getElementById('text-area-five-pm').innerHTML = itemsFive
+}
+
